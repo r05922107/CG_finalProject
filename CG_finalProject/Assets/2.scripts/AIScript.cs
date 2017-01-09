7efd243d4a2978378dddd94f572705ed95eb574e
Brian <b15132555@gmail.com>
2017-01-10 01:53:10 +0800

    add main character's model

---
+++ CG_finalProject/Assets/2.scripts/AIScript.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript : MonoBehaviour {
+	
+	// Objects
+	public GameObject playerObject;
+	public GameObject enemyObject;
+
+	// properties
+	public float enemyHP = 100f;
+	public int enemySpeed = 1;
+	public float enemyAttackCD = 3;
+	public int enemyRotateSpeed = 1;
+
+	// variables
+	private float attackCD;
+
+
+	// Use this for initialization
+	void Start () {
+		attackCD = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// update attackCD
+		attackCD = attackCD - Time.smoothDeltaTime <= 0 ? 0 : attackCD - Time.smoothDeltaTime;
+
+		// check player exist
+		if (playerObject == null) {
+			return;
+		}
+
+		// compute forward and direction vector to player 
+		Vector3 forward = enemyObject.transform.forward;
+		Vector3 direction = (playerObject.transform.position - enemyObject.transform.position);
+
+		// compute angle and distance to player
+		float distance = direction.sqrMagnitude;
+		float angle = angle_360(forward,direction);
+
+		// track
+		if (350 > angle && angle > 180) { // left rotate 
+			enemyObject.transform.Rotate (0, -1 * enemyRotateSpeed, 0);
+		} else if (180 >= angle && angle > 10) { // right rotate
+			enemyObject.transform.Rotate (0, enemyRotateSpeed, 0);
+		} else if (distance < 8) { // attack
+			if (attackCD > 0) { // return if CD-ing
+				return;
+			} else {
+				attack (playerObject, 0);
+			}
+		} else if (distance < 50000) { // move to player
+			enemyObject.transform.Translate (0, 0, enemySpeed * Time.smoothDeltaTime);
+		}
+	
+	}
+
+
+	private void attack(GameObject target, int attackType){
+		//hurt (playerObject, 10, 5);
+		print("Attack !\n");
+
+		// attack animation
+		// do some animation
+
+		// hurt target
+		// simple test
+		//target.transform.Translate (0, 0, -5);
+
+		// reset CD
+		attackCD = enemyAttackCD;
+	}
+
+
+	public void hurt(GameObject attacker, float damage, float knockback){
+		// decrease HP
+		enemyHP -= damage;
+
+		// be knockback 
+		Vector3 attackDirection = attacker.transform.forward.normalized;
+		enemyObject.transform.position = enemyObject.transform.position + knockback * attackDirection;
+
+		// check dead
+		if (enemyHP <= 0) { // if dead destroyed itself
+			Destroy (enemyObject);
+		}
+	}
+
+
+	private float angle_360(Vector3 from_, Vector3 to_){  
+		Vector3 v3 = Vector3.Cross(from_,to_);  
+		if(v3.y > 0)  
+			return Vector3.Angle(from_,to_);  
+		else  
+			return 360-Vector3.Angle(from_,to_);  
+	}
+		
+}
---
+++ CG_finalProject/Assets/2.scripts/mainChaAct.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainChaAct : MonoBehaviour {
+
+    public GameObject forwardPoint;
+    public GameObject hitBox;
+    public Rigidbody rigBody;
+    public Animator anim;
+    public float mSpeed = 0.1F;
+    public float rSpeed = 1;
+    public float mcAttackCD = 5;
+    public float HP = 100f;
+
+    private int counter = 0;
+    private float inputH;
+    private float inputV;
+    private float attackCD;
+    //private bool attack;
+
+    Vector3 fp;
+    float way = 75;
+    float c = 1;
+    
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.Play("idle", -1, 0f);
+        inputH = Input.GetAxis("Horizontal");//獲取水平軸向按鍵
+        inputV = Input.GetAxis("Vertical");//獲取垂直軸向按鍵
+        anim.SetFloat("InputH", inputH);
+        anim.SetFloat("InputV", inputV);
+        transform.Translate(0, 0, mSpeed * inputV, Space.World);
+        transform.Translate(mSpeed * inputH, 0, 0, Space.World);
+        
+
+        fp = forwardPoint.transform.position;
+        Vector2 dir2 = new Vector2(fp.x - transform.position.x, fp.z - transform.position.z);
+        float angle = Vector2.Angle(dir2, new Vector2(inputH, inputV));
+
+        if ((inputH != 0 || inputV != 0) && angle > 5)
+        {
+            if (angle - way > 1)
+            {
+                c = c * (-1);
+                Debug.Log("reverse!!!!!");
+            }
+            transform.transform.Rotate(0, rSpeed * c, 0);
+            way = angle;
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
+            anim.Play("Attack1");
+            hitBox.SetActive(true);
+            counter = 30;
+        }
+
+        if (counter > 0)
+        {
+            counter--;
+        }
+
+        if (counter == 0)
+        {
+            hitBox.SetActive(false);
+        }
+        //float moveX = inputH * 50f * Time.deltaTime;
+        //float moveZ = inputV * 50f * Time.deltaTime;
+
+        //rigBody.velocity = new Vector3(moveX, 0f, moveZ);
+
+
+        //transform.RotateAround(transform.TransformPoint(transform.position), Vector3.up, 20 * Time.deltaTime);
+        //transform.Translate(0, 0, mSpeed * v);//根據水平軸向按鍵來前進或後退
+        //transform.Translate(mSpeed * h, 0, 0);//
+
+    }
+
+
+    public void hurt(GameObject attacker, float damage, float knockback)
+    {
+        // decrease HP
+        HP -= damage;
+
+        // be knockback 
+        Vector3 attackDirection = attacker.transform.forward.normalized;
+        this.transform.position = this.transform.position + knockback * attackDirection;
+
+        // check dead
+        if (HP <= 0)
+        { // if dead destroyed itself
+            Destroy(this);
+        }
+    }
+
+}
