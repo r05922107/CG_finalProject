9d71d6e73fe69bf77adee4fc6ab070342c4f9370
brianlee <brian50208lee@gmail.com>
2017-01-09 19:24:00 +0800

    update AIScript

--- CG_finalProject/Assets/Scripts/AIScript.cs
+++ CG_finalProject/Assets/Scripts/AIScript.cs
@@ -11,7 +11,7 @@ public class AIScript : MonoBehaviour {
 	public float enemyHP = 100f;
 	public int enemySpeed = 1;
 	public float enemyAttackCD = 3;
-	private int enemyRotateSpeed = 1;
+	public int enemyRotateSpeed = 1;
 
 	// variables
 	private float attackCD;
@@ -19,7 +19,7 @@ public class AIScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		attackCD = enemyAttackCD;
+		attackCD = 0;
 	}
 	
 	// Update is called once per frame
@@ -27,45 +27,74 @@ public class AIScript : MonoBehaviour {
 		// update attackCD
 		attackCD = attackCD - Time.smoothDeltaTime <= 0 ? 0 : attackCD - Time.smoothDeltaTime;
 
-		// compute direction vector to player 
+		// check player exist
+		if (playerObject == null) {
+			return;
+		}
+
+		// compute forward and direction vector to player 
 		Vector3 forward = enemyObject.transform.forward;
 		Vector3 direction = (playerObject.transform.position - enemyObject.transform.position);
+
+		// compute angle and distance to player
 		float distance = direction.sqrMagnitude;
+		float angle = angle_360(forward,direction);
 
-		Vector2 forward2D = new Vector2 (forward.x, forward.z).normalized;
-		Vector2 direction2D = new Vector2 (direction.x, direction.z).normalized;
-
-		float rotate = Vector3.Cross (forward2D, direction2D).z;
-		float face = Vector3.Dot (forward2D, direction2D);
-
-		//track
-		if (rotate > 0.05) { // right rotate 
-			enemyObject.transform.Rotate (0, enemyRotateSpeed, 0);
-		} else if (rotate < -0.05) { // left rotate
+		// track
+		if (350 > angle && angle > 180) { // left rotate 
 			enemyObject.transform.Rotate (0, -1 * enemyRotateSpeed, 0);
-		} else if (distance < -1) { // attack
+		} else if (180 >= angle && angle > 10) { // right rotate
+			enemyObject.transform.Rotate (0, enemyRotateSpeed, 0);
+		} else if (distance < 8) { // attack
 			if (attackCD > 0) { // return if CD-ing
 				return;
+			} else {
+				attack (playerObject, 0);
 			}
-			// 
-			//Rigidbody shoot = (Rigidbody)Instantiate (bullet, firePoint.transform.position, firePoint.transform.rotation);
-			//shoot.velocity = direction;
-			//Physics.IgnoreCollision (firePoint.transform.root.GetComponent<Collider> (), shoot.GetComponent<Collider> ());
-			print("Attack !\n");
-			attackCD = enemyAttackCD;
 		} else if (distance < 50000) { // move to player
-			enemyObject.transform.Translate (0, 0, (-1) * enemySpeed * Time.smoothDeltaTime);
+			enemyObject.transform.Translate (0, 0, enemySpeed * Time.smoothDeltaTime);
 		}
 	
 	}
 
-	public int hurt(GameObject attacker, float damege){
-		// enemyHP deacrease by damage
-		print(attacker.gameObject.tag + " attack " + damege + " damage.\n");
-		enemyHP -= damege;
-		print("Remined HP :" + enemyHP + "\n");
-		return 1;
+
+	private void attack(GameObject target, int attackType){
+		//hurt (playerObject, 10, 5);
+		print("Attack !\n");
+
+		// attack animation
+		// do some animation
+
+		// hurt target
+		// simple test
+		//target.transform.Translate (0, 0, -5);
+
+		// reset CD
+		attackCD = enemyAttackCD;
 	}
 
 
+	public void hurt(GameObject attacker, float damage, float knockback){
+		// decrease HP
+		enemyHP -= damage;
+
+		// be knockback 
+		Vector3 attackDirection = attacker.transform.forward.normalized;
+		enemyObject.transform.position = enemyObject.transform.position + knockback * attackDirection;
+
+		// check dead
+		if (enemyHP <= 0) { // if dead destroyed itself
+			Destroy (enemyObject);
+		}
+	}
+
+
+	private float angle_360(Vector3 from_, Vector3 to_){  
+		Vector3 v3 = Vector3.Cross(from_,to_);  
+		if(v3.y > 0)  
+			return Vector3.Angle(from_,to_);  
+		else  
+			return 360-Vector3.Angle(from_,to_);  
+	}
+		
 }
