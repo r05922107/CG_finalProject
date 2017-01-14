85cc3c578ef516e9c99106b971afc9b3d987d45e
brianlee <brian50208lee@gmail.com>
2017-01-14 20:18:14 +0800

    add Resources folder add popupText prefab add popupText script

--- CG_finalProject/Assets/survival_on_the_square/script/FloatingTextController.cs
+++ CG_finalProject/Assets/survival_on_the_square/script/FloatingTextController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HPBar : MonoBehaviour {
+public class FloatingTextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +10,6 @@ public class HPBar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Camera.main.transform.rotation;
+	
 	}
 }
